13d441daddd2efc47a54beaf8dd3bb2f738b325a
Gereltuya ALTANGEREL <galtange@e2r2p5.clusters.42paris.fr>
2022-09-20 20:57:27 +0200

    changed ft_char2int

--- srcs/main.c
+++ srcs/main.c
@@ -1,10 +1,70 @@
 #include "../include/push_swap.h"
 
+int	ft_isalpha(char c)
+{
+	if (('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
+
+long long	ft_atoi(const char *nptr)
+{
+	int		i;
+	int		sign;
+	long long	num;
+
+	i = 0;
+	num = 0;
+	sign = 1;
+
+
+	while ((nptr[i] > 7 && nptr[i] < 14) || (nptr[i] == 32))
+		i++;
+	while (nptr[i] == '-' || nptr[i] == '+')
+	{
+		if (nptr[i] == '-')
+			sign = -1 * sign;
+		i++;
+	}
+	while (47 < nptr[i] && nptr[i] < 58)
+	{
+		num = 10 * num + (nptr[i] - '0');
+		i++;
+	}
+	return (num * sign);
+}
+
+int	ft_char2int(int *numbers, char *all)
+{
+	int	i = 0;
+	long long	nbr;
+	char	**split;
+	
+	split = ft_split(all, ' ');
+	while(split)
+	{
+		if (ft_isalpha(split[i][0]) == 0)
+		{
+			ft_free(split, i);
+			free(numbers);
+			return (1);
+		}
+		nbr = ft_atoi(split[i]);
+		if (2147483647 < nbr || nbr < -2147483648)
+			return (1);
+		numbers[i] = nbr;
+		i++;
+	}
+	return (0);
+}
+
 int main(int argc, char	**argv)
 {
 	char	*all;
-	char	**split;
 	int		i = 0;
+	int		*numbers = NULL;
+
 	if (argc >= 1)
 	{
 		all = ft_joinall(argc - 1, argv + 1);
@@ -13,16 +73,21 @@ int main(int argc, char	**argv)
 			free(all);
 			ft_putstr_fd("Error\n", 1);
 		}
-		split = ft_split(all, ' ');
-		if (!split)
+		numbers = malloc((argc - 1) * sizeof(int));
+		if (!numbers)
 		{
-			ft_free(split, argc - 1);
-			ft_putstr_fd("Error\n", 1);
+			free(numbers);
+			return (0);
 		}
-		while(i < (argc - 1))
+		if(ft_char2int(numbers, all) == 1)
 		{
-			ft_putstr_fd(split[i], 1);
-			ft_putstr_fd("\n", 1);
+			free(all);
+			ft_putstr_fd("error\n", 1);
+			return (0);
+		}		
+		while (i < argc - 1)
+		{
+			printf("%d\n", numbers[i]);
 			i++;
 		}
 	}
