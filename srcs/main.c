dc4b5596387d3c55bbbdce94dc76beb9479e7016
Gereltuya ALTANGEREL <gereltuya.altangerel@gmail.com>
2022-09-22 18:37:36 +0200

    int check ok

--- srcs/main.c
+++ srcs/main.c
@@ -27,7 +27,7 @@ long long	ft_atoi(const char *nptr)
 			sign = -1 * sign;
 		i++;
 	}
-	while (47 < nptr[i] && nptr[i] < 58)
+	while ('0' <= nptr[i] && nptr[i] <= '9')
 	{
 		num = 10 * num + (nptr[i] - '0');
 		i++;
@@ -35,65 +35,131 @@ long long	ft_atoi(const char *nptr)
 	return (num * sign);
 }
 
-int	ft_char2int(int *numbers, char *all)
+int	*ft_char2int(char **tmp, int size)
 {
 	int	i = 0;
-	long long	nbr;
-	char	**split;
-	
-	split = ft_split(all, ' ');
-	while(split)
+	int	*nbrs;
+	long long	nb = 0;	
+
+	nbrs = (int *)malloc(size * sizeof(int));
+	if (!nbrs || !tmp)
+		return (NULL);
+	while (tmp[i] && i < size)
 	{
-		if (ft_isalpha(split[i][0]) == 0)
+		nb = ft_atoi(tmp[i]);
+		if (nb > 2147483647 || nb < -2147483648)
 		{
-			ft_free(split, i);
-			free(numbers);
-			return (1);
+			free(nbrs);
+			return (NULL);
 		}
-		nbr = ft_atoi(split[i]);
-		if (2147483647 < nbr || nbr < -2147483648)
-			return (1);
-		numbers[i] = nbr;
+		nbrs[i] = (int)nb;
 		i++;
 	}
-	return (0);
+	return (nbrs);
 }
 
+char	**ft_split2(int	size, char **nbrs)
+{
+	char	**result;
+	int	line;
+
+	result = (char **)malloc((size + 1) * sizeof(char *));
+	if (!nbrs || !result)
+		return(NULL);
+	line = 0;
+	while (line < size)
+	{
+		result[line] = ft_substr(nbrs[line], 0, ft_strlen(nbrs[line]));
+		if (!result[line] || ft_isalpha(result[line][0]))
+		{
+			ft_free(result, line);
+			return (NULL);
+		}
+		line++;
+	}
+	result[line] = NULL;
+	return (result);
+}
+
+int	ft_dupcheck(int *nbrs, int size)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while(i < size)
+	{
+		j = i + 1;
+		while (j < size)
+		{
+			if (nbrs[i] == nbrs[j])
+				return 0;
+			j++;
+		}
+		i++;
+	}
+	return (1);
+}
+
+int main (int argc, char **argv)
+{
+	char	**all;
+	int	*nbrs;
+	int	i = 0;
+	
+	if (argc >= 1)
+	{
+		all = ft_split2(argc - 1, argv + 1);
+		if (!all)
+			return (write(1, "Error\n", 6));
+		nbrs = ft_char2int(all, argc - 1);
+		if (!nbrs || !ft_dupcheck(nbrs, argc - 1))
+			return (write(1, "Error\n", 6));
+		i = 0;
+		while (i < (argc - 1))
+			printf("%d\n", nbrs[i++]);
+		free(nbrs);
+		ft_free(all, i);
+	}
+}
+
+/* ver2
 int main(int argc, char	**argv)
 {
 	char	*all;
 	int		i = 0;
-	int		*numbers = NULL;
+	char	**tmp;
+	int	*nbrs;
 
 	if (argc >= 1)
 	{
 		all = ft_joinall(argc - 1, argv + 1);
 		if (!all)
 		{
-			free(all);
+			free(all);i
 			ft_putstr_fd("Error\n", 1);
 		}
-		numbers = malloc((argc - 1) * sizeof(int));
-		if (!numbers)
-		{
-			free(numbers);
+		tmp = ft_split(all, ' ');
+		while (tmp[i])
+		{	
+			ft_putstr_fd(tmp[i++], 1);
+			ft_putstr_fd("\n", 1);
+		
+		nbrs = (int *)malloc((i + 1) * sizeof(int));
+		if (!nbrs)
 			return (0);
-		}
-		if(ft_char2int(numbers, all) == 1)
-		{
-			free(all);
-			ft_putstr_fd("error\n", 1);
-			return (0);
-		}		
-		while (i < argc - 1)
-		{
-			printf("%d\n", numbers[i]);
-			i++;
-		}
+		ft_char2int(tmp, nbrs);
+		i = 0;
+		while (nbrs[i])
+			printf("%d\n", nbrs[i++]);
+		free(all);
+		ft_free(tmp, i);
+		free(nbrs);
+
 	}
 	return (0);
 }
-
+*/
 // int main(int argc, char **argv)
 // {
 // 	static int	tmp[10000];
